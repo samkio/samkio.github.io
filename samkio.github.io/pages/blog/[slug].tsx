3005967333678a834acad5dcad367cd2b0ce9669
samkio <samkio@users.noreply.github.com>
2023-03-23 21:44:39 +0000

    Adapt blog page

--- samkio.github.io/pages/blog/[slug].tsx
+++ samkio.github.io/pages/blog/[slug].tsx
@@ -1,4 +1,3 @@
-import ContentWrapper from "@/components/ContentWrapper";
 import { GetStaticPaths, GetStaticProps } from "next";
 import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
 import Head from "next/head";
@@ -7,6 +6,41 @@ import { POSTS_PATH, postFilePaths, BlogMatter } from "@/utils/mdxUtils";
 import path from "path";
 import fs from "fs";
 import matter from "gray-matter";
+import SlimHeader from "@/components/SlimHeader";
+import styled from "@emotion/styled";
+
+const ContentCenter = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ContentWrapper = styled.div`
+  max-width: 1000px;
+  margin-top: 35px;
+  align-items: center;
+  background-color: #ffffff;
+  border-left: 5px solid #000000;
+  border-right: 5px solid #000000;
+`;
+
+const BannerContainer = styled.div`
+  position: relative;
+`;
+
+const BannerImage = styled.img`
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+  object-position: 0 20%;
+  filter: grayscale(100%) blur(3px);
+`;
+
+const BlogTitle = styled.h1`
+  position: absolute;
+  bottom: 0px;
+  left: 10px;
+`;
 
 type BlogPostProps = {
   source: MDXRemoteSerializeResult;
@@ -23,11 +57,17 @@ export default function BlogPost({ source, frontMatter }: BlogPostProps) {
           content="Samkio's site. A place where I can share my ideas and creativity to the world."
         />
       </Head>
-      <ContentWrapper>
-        <h1>{frontMatter.title}</h1>
-        {frontMatter.description && <h2>{frontMatter.description}</h2>}
-        <MDXRemote {...source} />
-      </ContentWrapper>
+      <SlimHeader />
+      <ContentCenter>
+        <ContentWrapper>
+          <BannerContainer>
+            <BannerImage src={"/images/blog/crumble.jpg"} />
+            <BlogTitle>{frontMatter.title}</BlogTitle>
+          </BannerContainer>
+          {frontMatter.description && <h2>{frontMatter.description}</h2>}
+          <MDXRemote {...source} />
+        </ContentWrapper>
+      </ContentCenter>
     </>
   );
 }
