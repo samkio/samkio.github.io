f834d1255e717ce76fa69ff91ad26849630aef70
samkio <samkio@users.noreply.github.com>
2023-04-21 19:43:05 +0100

    Add Trophy SVG

--- samkio.github.io/pages/projects/gaming/index.tsx
+++ samkio.github.io/pages/projects/gaming/index.tsx
@@ -14,12 +14,52 @@ export default function Projects() {
       <ContentWrapper>
         <h1>Game Trophy Cabinet</h1>
         <svg
-          style={{ border: "1px solid black" }}
-          height="550px"
+          height="600px"
           xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 100 100"
+          viewBox="0 0 600 600"
         >
-          <circle cx="50" cy="30" r="25" />
+          <circle cx="300" cy="170" r="130" />
+          {/* <path
+            d="M 140 120 Q 110 220 200 300 S 240 330 260 370 S 270 420 260 470 L 270 540 L 200 540 Q 240 510 230 440 S 220 360 150 310 S 70 230 140 120 z"
+            strokeWidth="1"
+            fill="transparent"
+            stroke="red"
+          /> */}
+          <path
+            d="M 170 120 C 120 330 340 270 290 460 L 300 510 L 300 550 L 240 550 C 320 310 50 340 170 120 z"
+            strokeWidth="10"
+            fill="black"
+            stroke="black"
+            strokeLinejoin="round"
+          ></path>
+          <path
+            d="M 430 120 C 480 330 260 270 310 460 L 300 510 L 300 550 L 360 550 C 280 310 550 340 430 120 z"
+            strokeWidth="10"
+            fill="black"
+            stroke="black"
+            strokeLinejoin="round"
+          ></path>
+          <path
+            d="M 110 310 Q 260 340 250 550 L 220 550 Q 220 410 110 310 Z"
+            strokeWidth="10"
+            fill="black"
+            stroke="black"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M 492 310 Q 342 340 352 550 L 382 550 Q 382 410 492 310 Z"
+            strokeWidth="10"
+            fill="black"
+            stroke="black"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M 400 530 L 410 570 Q 300 590 190 570 L 200 530 Z"
+            strokeWidth="10"
+            fill="black"
+            stroke="black"
+            strokeLinejoin="round"
+          />
         </svg>
       </ContentWrapper>
     </>
